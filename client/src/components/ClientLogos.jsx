ee37af1199f64e93b0281f3aebf4b067af778111
Dharmu Kumar <project.689.test@gmail.com>
2026-04-02 23:53:06 +0530

    Update ClientLogos.jsx

--- client/src/components/ClientLogos.jsx
+++ client/src/components/ClientLogos.jsx
@@ -2,7 +2,8 @@ import { motion } from 'framer-motion';
 import { fadeInUp, staggerContainer } from '../hooks/useScrollAnimation';
 
 const clients = [
-  { name: "Neokraft Global", sub: "CLIENT" },
+  {},
+  { name: "Neokraft Global", sub: "CLIENT", Image:"LOGO.png" },
   { name: "Worlds of Wonder", sub: "AMUSEMENT PARK" },
   { name: "The Great India Place", sub: "MALL" },
   { name: "Hindustan Amusement Machines", sub: "MACHINES" },
